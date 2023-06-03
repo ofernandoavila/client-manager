115acd0f032b1f1d82ea1e80519d112a311bb914
Fernando <fernandoavilajunior@gmail.com>
2023-06-03 13:44:21 -0300

    doctrine binary created

---
+++ api/bin/doctrine.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+use Doctrine\ORM\Tools\Console\EntityManagerProvider\SingleManagerProvider;
+use Avilamidia\ClientManager\Helper\EntityManagerCreator;
+
+global $applicationMode;
+
+$applicationMode = 'dev';
+
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../src/config');
+$dotenv->load();
+
+$entityManager = EntityManagerCreator::createEntityManager();
+
+ConsoleRunner::run(
+    new SingleManagerProvider($entityManager)
+);
