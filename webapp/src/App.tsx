2ecb0c9915f6b1cd868fe538b5dcaf50616c57e0
Fernando Avila <fernandoavilajunior@gmail.com>
2023-06-04 04:44:42 -0300

    new routes

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./views/Home";
 import { ClientsView } from './views/client/ClientsView';
 import CreateNewClientView from './views/client/CreateNewClientView';
+import { OrdersView } from './views/orders/OrdersView';
+import CreateNewOrderView from './views/orders/CreateNewOrderView';
 
 function App() {
   return (
@@ -9,7 +11,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path='/clients' element={<ClientsView />} />
+        <Route path='/orders' element={<OrdersView />} />
         <Route path='/clients/new' element={<CreateNewClientView />} />
+        <Route path='/orders/new' element={<CreateNewOrderView />} />
       </Routes>
     </Router>
   );
