85f1d5f6d98194eb7f23196d64657acb714908bd
Fernando Avila <fernandoavilajunior@gmail.com>
2023-06-04 21:55:48 -0300

    routes updated

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -5,6 +5,13 @@ import CreateNewClientView from './views/client/CreateNewClientView';
 import { OrdersView } from './views/orders/OrdersView';
 import CreateNewOrderView from './views/orders/CreateNewOrderView';
 import ViewOrder from './views/orders/ViewOrder';
+import ConfigurationsView from './views/configurations/ConfigurationsView';
+import UsersView from './views/configurations/users/UsersView';
+import PreferencesView from './views/configurations/preferences/PreferencesView';
+import PreferencesNewView from './views/configurations/preferences/PreferencesNewView';
+import PreferencesEditView from './views/configurations/preferences/PreferencesEditView';
+import UsersNewView from './views/configurations/users/UsersNewView';
+import UsersEditView from './views/configurations/users/UsersEditView';
 
 function App() {
   return (
@@ -13,6 +20,13 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path='/clients' element={<ClientsView />} />
         <Route path='/orders' element={<OrdersView />} />
+        <Route path='/configurations' element={<ConfigurationsView />} />
+        <Route path='/configurations/users' element={<UsersView />} />
+        <Route path='/configurations/users/new' element={<UsersNewView />} />
+        <Route path='/configurations/users/edit/:userHash' element={<UsersEditView />} />
+        <Route path='/configurations/preferences' element={<PreferencesView />} />
+        <Route path='/configurations/preferences/new' element={<PreferencesNewView />} />
+        <Route path='/configurations/preferences/edit/:preferenceSlug' element={<PreferencesEditView />} />
         <Route path='/clients/new' element={<CreateNewClientView />} />
         <Route path='/orders/new' element={<CreateNewOrderView />} />
         <Route path='/orders/:orderId' element={<ViewOrder />} />
