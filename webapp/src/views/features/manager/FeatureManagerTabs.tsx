b69082fc3b925a5582cce10c683f627ca227364d
Fernando Avila <fernandoavilajunior@gmail.com>
2023-06-07 02:11:19 -0300

    button added

--- webapp/src/views/features/manager/FeatureManagerTabs.tsx
+++ webapp/src/views/features/manager/FeatureManagerTabs.tsx
@@ -1,3 +1,5 @@
+import { Form } from "react-bootstrap";
+import Modal from "../../../components/Modal";
 import TabNavigator from "../../../components/TabNavigator";
 
 export interface FeatureManagerTabsProps {
@@ -30,6 +32,23 @@ export function FeatureManagerSettingsTab(props: FeatureManagerTabsProps) {
         <>
             <div className="d-flex justify-content-between mb-4">
                 <h3 className="">Settings</h3>
+            </div>
+            <div className="media text-muted pt-3">
+                <div className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                    <div className="d-flex justify-content-between align-items-center w-100">
+                        <strong className="text-gray-dark">Display feature at header menu</strong>
+                        <Form.Check // prettier-ignore
+                            type="switch"
+                            id="custom-switch"
+                        />
+                        <Modal
+                            isOpen={false}
+                            onConfirmLabel={"Reset all preferences"}
+                            text="Do you really want to reset all preferences?"
+                            type="Warning"
+                        />  
+                    </div>
+                </div>
             </div>
         </>
     );
