9074b983a60f8911bc36c3859ccd1fdec2b0bf39
Fernando Avila <fernandoavilajunior@gmail.com>
2023-06-10 22:56:03 -0300

    site title fix

--- webapp/src/components/MenuHeader.tsx
+++ webapp/src/components/MenuHeader.tsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { PreferencesHelper } from '../helpers/PreferencesHelper';
+import { usePreference } from '../hooks/usePreference';
 
 export default function MenuHeader() {
 
-    const [title, setTitle] = useState('Client Manager');
+    const { GetPreferenceValue } = usePreference();
 
     return(
         <div className="navbar navbar-dark bg-dark text-light shadow-sm sticky-top">
             <div className="container d-flex justify-content-between">
             <div className="navbar-brand d-flex align-items-center">
-                <Link className="p-2 text-decoration-none text-white" to={'/'}><strong>{ title }</strong></Link>
+                <Link className="p-2 text-decoration-none text-white" to={'/'}><strong>{ GetPreferenceValue('corporation-name') ?? "Company Manager" }</strong></Link>
             </div>
             <nav className="my-2 my-md-0 mr-md-3">
                 <Link className="p-2 text-decoration-none text-white" to={'/clients'}>Clients</Link>
