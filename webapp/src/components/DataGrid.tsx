c0b5ac98136cc6dd2db46175524819a57833eaf7
Fernando Avila <fernandoavilajunior@gmail.com>
2023-06-10 22:58:59 -0300

    DataGrid upgraded with ignore and formatters

--- webapp/src/components/DataGrid.tsx
+++ webapp/src/components/DataGrid.tsx
@@ -1,16 +1,40 @@
 import { useEffect, useState } from "react";
+import { PreferencesHelper } from "../helpers/PreferencesHelper";
+import useFormatter from "../helpers/Formatter";
+import { GenericObjectType } from "../types/ContextTypes";
 
-interface DataGridProps<T> extends DataGridGenericObject {
+interface DataGridProps<T> extends GenericObjectType {
     objects: T[];
+    options?: DataGridPropsOptions;
 }
 
-interface DataGridGenericObject {
-    [key: string]: any;
+interface DataGridPropsOptions {
+    formatProperty?: FormatProperty[];
+    ignoreProperties?: string[];
 }
 
-export default function DataGrid<T extends DataGridGenericObject>(props: DataGridProps<T>) {
+interface FormatProperty {
+    property: string;
+    type: "currency" | "string";
+    stringFormat?: StringFormatProperty;
+}
+
+interface IgnoreProperty {
+    name: string;
+}
+
+interface StringFormatProperty {
+    format: string;
+    valuesKeys: string[];
+}
+
+export default function DataGrid<T extends GenericObjectType>(props: DataGridProps<T>) {
     
     const [ keys, setKeys ] = useState<string[]>([]);
+    const currency = PreferencesHelper.GetPreference('currency');
+    const decimalSeparator = PreferencesHelper.GetPreference('decimal-separator');
+
+    const { Currency, StringFormatter, CamelToString } = useFormatter();
     
     useEffect(() => {
         if(props.objects[0] !== null && props.objects[0] !== undefined) {
@@ -25,13 +49,63 @@ export default function DataGrid<T extends DataGridGenericObject>(props: DataGri
         <table className="table">
             <thead>
                 <tr>
-                    { keys.map( (key) => <th key={key} scope="col">{ key }</th>) }
+                    { keys.map( (key) => {
+                        if(props.options !== null && props.options !== undefined) {
+                            if(props.options.ignoreProperties !== null && props.options.ignoreProperties !== undefined && props.options.ignoreProperties.length > 0) {
+                                let ignoreProperty;
+                                props.options.ignoreProperties.forEach( ignore => {
+                                    if(key == ignore) {
+                                        ignoreProperty = true;
+                                        return;
+                                    }
+                                } );
+                                if(ignoreProperty) return;
+                            }
+                        }
+                        return (
+                            <th key={key}>{CamelToString(key)}</th>
+                        )
+                    }) }
                 </tr>
             </thead>
             <tbody>
                 { props.objects.map ( (item , i) => (
                     <tr key={i}>
-                        { keys.map( (key) => <td key={key}>{item[key]}</td>) }
+                        { keys.map( (key) => {
+                            let value = item[key];
+
+                            if(props.options !== null && props.options !== undefined) {
+                                if(props.options.ignoreProperties !== null && props.options.ignoreProperties !== undefined && props.options.ignoreProperties.length > 0) {
+                                    let ignoreProperty;
+                                    props.options.ignoreProperties.forEach( ignore => {
+                                        // console.log(`comparing ${key} to ${ignore} : ${key == ignore}`);
+                                        if(key == ignore) {
+                                            ignoreProperty = true;
+                                            return;
+                                        }
+                                    } );
+                                    if(ignoreProperty) return <></>;
+                                }
+
+                                if(props.options.formatProperty !== null && props.options.formatProperty !== undefined && props.options.formatProperty.length > 0) {
+                                    if(props.options.formatProperty.find( (prop) => prop.property === key) !== undefined) {
+                                        switch(props.options.formatProperty.find( (prop) => prop.property === key)?.type) {
+                                            case 'currency':
+                                                value = Currency(value)
+                                                console.log(value);
+                                                break;
+
+                                            case 'string':
+                                                let formatter = props.options.formatProperty.find( (prop) => prop.property === key)!;
+                                                value = StringFormatter(formatter.stringFormat!.format, item, formatter.stringFormat!.valuesKeys);
+                                                break;
+                                        }
+                                    }
+                                }
+                            } 
+                            
+                            return <td key={key}>{value}</td>;
+                        }) }
                     </tr>
                 ))}
             </tbody>
